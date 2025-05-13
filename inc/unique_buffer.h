0b9e47d0fca672116f18329f3271132b8de6d9aa
Patrick Godwin <pbgodwin@xbox.com>
2025-05-12 23:44:25 -0700

    Refactor UniqueBuffer class and enhance CMake configuration for better build support

--- inc/unique_buffer.h
+++ inc/unique_buffer.h
@@ -15,12 +15,44 @@ Why: shows RAII, move semantics, and memory hygiene in one go.
 template <typename T>
 class UniqueBuffer {
     public:
-        UniqueBuffer<T>(const UniqueBuffer<T>& copy);
-        UniqueBuffer<T>(UniqueBuffer<T>&& src);
-        ~UniqueBuffer<T>();
+        UniqueBuffer<T>() {
+            m_size = 0;
+            m_buffer = nullptr;
+        }
 
-        UniqueBuffer<T> operator=(const UniqueBuffer<T>& copy);
-        UniqueBuffer<T> operator=(UniqueBuffer<T>&& src);
-    private:
+        UniqueBuffer<T>(size_t size) {
+            m_buffer = std::make_unique<T[]>(size);
+            m_size = size;
+        }
         
+        UniqueBuffer<T>(UniqueBuffer<T>&& src) noexcept {
+            m_buffer = std::move(src.m_buffer);
+            m_size = src.m_size;
+            src.m_buffer = nullptr;
+            src.m_size = 0;
+        }
+
+        ~UniqueBuffer<T>() {
+            m_size = 0;
+        }
+
+        UniqueBuffer<T>& operator=(UniqueBuffer<T>&& src) noexcept {
+            m_buffer = std::move(src.m_buffer);            
+            m_size = src.m_size;
+            src.m_buffer = nullptr;
+            src.m_size = 0;
+            return *this;
+        }
+
+        // prevent copies
+        UniqueBuffer<T> operator=(const UniqueBuffer<T>& copy) = delete;
+        UniqueBuffer<T>(const UniqueBuffer<T>& copy) = delete;
+
+    private:
+        size_t m_size;
+        std::unique_ptr<T[]> m_buffer;
+
+    public:
+        const size_t size() { return m_size; }
+        const T* data() { return m_buffer.get(); }
 };
--- tests/unique_buffer_tests.cpp
+++ tests/unique_buffer_tests.cpp
@@ -3,6 +3,32 @@
 #include <unique_buffer.h>
 
 TEST_CASE("Default ctor yields empty buffer") {
-    REQUIRE(1 == 2);
+    UniqueBuffer<int> m_buffer;
+    REQUIRE( m_buffer.size() == 0 );
+    REQUIRE( m_buffer.data() == nullptr);
 }
 
+TEST_CASE("Initial size ctor allocates buffer") {
+    UniqueBuffer<int> m_buffer(2);
+    REQUIRE( m_buffer.size() == 2 );
+    REQUIRE_FALSE( m_buffer.data() == nullptr );
+}
+
+TEST_CASE("Move ctor leaves source null") {
+    UniqueBuffer<int> m_srcBuffer(1);
+    UniqueBuffer<int> m_dest = std::move(m_srcBuffer);
+    REQUIRE( m_srcBuffer.size() == 0 );
+    REQUIRE( m_srcBuffer.data() == nullptr );
+    REQUIRE( m_dest.size() == 1 );
+    REQUIRE_FALSE( m_dest.data() == nullptr );
+}
+
+TEST_CASE("Move assignment leaves source null") {
+    UniqueBuffer<int> m_srcBuffer(1);
+    UniqueBuffer<int> m_dest;
+    m_dest = std::move(m_srcBuffer);
+    REQUIRE( m_srcBuffer.size() == 0 );
+    REQUIRE( m_srcBuffer.data() == nullptr );
+    REQUIRE( m_dest.size() == 1 );
+    REQUIRE_FALSE( m_dest.data() == nullptr );
+}
