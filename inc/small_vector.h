252cd0faf2d741a3ea503fde6fd813c32af39610
Patrick Godwin <pbgodwin@xbox.com>
2025-05-18 21:29:55 -0700

    fix: add empty check before destructing elements in move constructor and assignment

--- inc/small_vector.h
+++ inc/small_vector.h
@@ -47,8 +47,10 @@ class SmallVector {
         }
 
         SmallVector<T, N>(SmallVector<T,N>&& src) noexcept {
-            for (const T* it = begin(); it != end(); ++it) {
-                it->~T();
+            if (!empty()) {
+                for (const T* it = begin(); it != end(); ++it) {
+                    it->~T();
+                }
             }
 
             if (src.m_on_stack) {
@@ -65,8 +67,10 @@ class SmallVector {
         }
 
         SmallVector<T, N>& operator=(SmallVector<T,N>&& src) noexcept {
-            for (const T* it = begin(); it != end(); ++it) {
-                it->~T();
+            if (!empty()) {
+                for (const T* it = begin(); it != end(); ++it) {
+                    it->~T();
+                }
             }
             
             if (src.m_on_stack) {
